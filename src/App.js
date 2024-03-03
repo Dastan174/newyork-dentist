36c487fd513fde23eddc226fca4c5f6a3d3586ee
Dastan174 <138676099+Dastan174@users.noreply.github.com>
2024-03-03 17:50:18 +0600

    верстка

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import Doctors from "./components/Doctors/Doctors";
 import Home from "./components/Home/Home";
+import Clinic from "./components/Info Clinic/Clinic";
+import Partners from "./components/Partners/Partners";
+import Reviews from "./components/Reviews/Reviews";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
-      
+      <Services />
+      <Doctors />
+      <Clinic />
+      <Partners />
+      <Reviews />
     </div>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,39 +1,10 @@
 import React from "react";
-import Teeth from "../Teeth/Teeth";
-import Index1 from "../header/index1";
-import Index2 from "../header/index2";
-import Index3 from "../header/index3";
-import Rating from "../rating/Rating";
-import Our from "../Our/Our";
-import News from "../news/News";
-import Form1Home from "../form1Home/Form1Home";
-import Form2Home from "../form2Home/Form2Home";
-import Doctor from "../DoctorMen/Doctor";
-import Footer from "../FooterEnd/Footer";
-import Footer2 from "../FooterEnd/Footer2";
-import Footer3 from "../FooterEnd/Footer3";
-import Footer4 from "../FooterEnd/Footer4";
+
 const Home = () => {
   return (
     <div>
-      <Index1 />
-      <Index2 />
-      <Index3 />
-      <Teeth />
-      <Rating />
-      <Our />
-      <News />
-      <Form1Home />
-      <Doctor />
-      <Form2Home />
-      <Footer />
-      <Footer2 />
-      <Footer3 />
-      <Footer4 />
     </div>
   );
 };
-//aziret
-//aziret2
-//aerstfyugihopj[]
+
 export default Home;
