fe9269d9556bf046f835116031bd576a1362adcf
Dastan174 <138676099+Dastan174@users.noreply.github.com>
2024-03-03 18:01:47 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import Doctors from "./components/Doctors/Doctors";
-import Home from "./components/Home/Home";
-import Clinic from "./components/Info Clinic/Clinic";
-import Partners from "./components/Partners/Partners";
-import Reviews from "./components/Reviews/Reviews";
-import Services from "./components/Services/Services";
+
 
 function App() {
   return (
     <div className="App">
-      <Services />
-      <Doctors />
-      <Clinic />
-      <Partners />
-      <Reviews />
+      
     </div>
   );
 }
